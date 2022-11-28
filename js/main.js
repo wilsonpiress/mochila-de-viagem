12eb1900fa7aa536c4a9e180a31c2f2917961447
wilsonpiress <wilsonpires.js@gmail.com>
2022-11-28 15:42:37 -0300

    atualizando itens no navegador e no localStorage.

--- js/main.js
+++ js/main.js
@@ -13,14 +13,27 @@ form.addEventListener("submit", (evento) => {
     const nome = evento.target.elements['nome']
     const quantidade = evento.target.elements['quantidade']
 
+    const existe = itens.find( elemento => elemento.nome === nome.value )
+
     const itemAtual = {
         "nome": nome.value,
         "quantidade": quantidade.value
     }
 
-    criaElemento(itemAtual)
+    if (existe) {
+        itemAtual.id = existe.id
 
-    itens.push(itemAtual)
+        atualizaElemento(itemAtual)
+
+        itens[existe.id] = itemAtual
+    } else {
+        itemAtual.id = itens.length
+
+        criaElemento(itemAtual)
+
+        itens.push(itemAtual)
+    }
+
 
     localStorage.setItem("item", JSON.stringify(itens))
 
@@ -34,11 +47,15 @@ function criaElemento(item) {
 
     const numeroItem = document.createElement('strong')
     numeroItem.innerHTML = item.quantidade
+    numeroItem.dataset.id = item.id
     novoItem.appendChild(numeroItem)
 
     novoItem.innerHTML += item.nome
 
     lista.appendChild(novoItem)
 
-  
+}
+
+function atualizaElemento(item) {
+    document.querySelector("[data-id='"+item.id+"']").innerHTML = item.quantidade
 }
